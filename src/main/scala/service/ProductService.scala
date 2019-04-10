7503061ce689587c824607cbdfe91303fc15ba85
tomasiturralde <tomas.iturralde@ing.austral.edu.ar>
2019-04-10 20:08:24 -0300

    added response to ping

--- src/main/scala/service/ProductService.scala
+++ src/main/scala/service/ProductService.scala
@@ -1,6 +1,6 @@
 package service
 
-import product.product.{NewProductRequest, ProductReply, ProductRequest, ProductServiceGrpc}
+import product.product.{NewProductRequest, PingReply, PingRequest, ProductReply, ProductRequest, ProductServiceGrpc}
 import repositories.ProductRepository
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -15,6 +15,11 @@ class ProductService (repo: ProductRepository) (implicit ec: ExecutionContext) e
 
   override def newProduct(request: NewProductRequest): Future[ProductReply] = {
     repo.create(request.name, request.description).map(p => ProductReply(p.id, p.name, p.description))
+  }
+
+  //implementar isActive (solo recibe el request y devuelve un reply con un string)
+  override def isActive(request: PingRequest): Future[PingReply] = {
+    Future.successful(PingReply("active"))
   }
 }
 
